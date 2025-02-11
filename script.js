bddd18f89b81d4a7405e4cc15015aeb346a06b4e
Sonali Patil <118295011+Sonali2314@users.noreply.github.com>
2025-02-11 02:59:49 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
